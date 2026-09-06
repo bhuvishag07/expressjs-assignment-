7ad1be037b6a8a18448601538d553c4d21f52d93
Bhuvisha Gohil <bhuvishagohil07@gmail.com>
2026-09-06 17:32:26 +0530

    Refactor server.js for improved readability

--- server.js
+++ server.js
@@ -1,43 +1,54 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-//Task 1 Basic Routes
+
+// Task 1: Basic Routes
+
 app.get('/', (req, res) => {
-    console.log(req.method, req.url)
-  res.send('Welcome to the Home Page');
+    console.log(req.method, req.url);
+    res.send('Welcome to the Home Page');
 });
 
 app.get('/about', (req, res) => {
-    console.log(req.method, req.url)
-  res.send('This is the About Page');
+    console.log(req.method, req.url);
+    res.send('This is the About Page');
 });
 
 app.get('/contact', (req, res) => {
-    console.log(req.method, req.url)
-  res.send('This is the Contact Page');
-}); 
-app.listen(port, () => {
-    console.log(`Server is running on port ${3000}`)
-})
-//Task 2 Route Parameters
+    console.log(req.method, req.url);
+    res.send('This is the Contact Page');
+});
+
+// Task 2: Route Parameters
+
 app.get('/user/:name', (req, res) => {
-    console.log(req.method, req.url)
-    const name = req.params.name
-    res.send(`Hello ${name}`)
-})
-//Task 3 Multiple Route Parameters
+    console.log(req.method, req.url);
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+// Task 3: Multiple Route Parameters
+
 app.get('/product/:id/:category', (req, res) => {
-    console.log(req.method, req.url)
-    const id = req.params.id
-    const category = req.params.category
+    console.log(req.method, req.url);
+    const id = req.params.id;
+    const category = req.params.category;
 
-    res.send(`Product ID: ${id}, Category: ${category}`)
-})
-//Task 4 Query Parameters
+    res.send(`Product ID: ${id}, Category: ${category}`);
+});
+
+// Task 4: Query Parameters
+
 app.get('/search', (req, res) => {
-    console.log(req.method, req.url)
-    const name = req.query.name
-    const role = req.query.role
+    console.log(req.method, req.url);
+    const name = req.query.name;
+    const role = req.query.role;
 
-    res.send(`Name: ${name}, Role: ${role}`)
-})
+    res.send(`Name: ${name}, Role: ${role}`);
+});
+
+// Start Server
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
